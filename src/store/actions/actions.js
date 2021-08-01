4922a4581096487837a68570673d67d5eb54dd9d
Николай Карнаев <karnaev.nick@gmail.com>
2021-08-01 22:50:54 +0300

    Добавляет страницу корзины

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -4,7 +4,7 @@ export const ActionType = {
   CHANGE_FILTER: `changeFilter`,
   CHANGE_QUANTITY_GUITARS: `changeQuantityGuitars`,
   APPLY_PROMOCODE: `applyPromocode`,
-};
+}
 
 export const ActionCreator = {
   addToCart: (item) => ({
@@ -31,4 +31,4 @@ export const ActionCreator = {
     type: ActionType.APPLY_PROMOCODE,
     payload: promocode,
   }),
-};
+}
